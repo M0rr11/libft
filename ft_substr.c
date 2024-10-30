e8e939e4e51d20df9e13156a4ac6b75ab002752a
Nyx <150604259+M0rr11@users.noreply.github.com>
2024-10-30 02:43:38 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -2,26 +2,20 @@
 
 char *ft_substr(const char *s, unsigned int start, size_t len)
 {
+    char *p;
     size_t i;
-    char *str;
     
-    if (!s)
-        return (NULL);
+    p = malloc(len + 1);
     
-    if (start > ft_strlen(s))
-        return (ft_strdup(""));
-    
-    if (len > ft_strlen(s + start))
-        len = ft_strlen(s + start);
-
-    str = ft_calloc(len + 1, sizeof(char));
-    if (!str)
-        return (NULL);
+    if (p == 0)
+		return (NULL);
     i = 0;
-    while (i < len)
-    {
-        str[i] = s[start + i];
-        i++;
-    }
-    return (str);
+    while(i <len)
+        {
+            p[i] = s[start];
+            i++;
+            start++;
+        }
+    p[i] = '\0';
+    return (p);
 }
