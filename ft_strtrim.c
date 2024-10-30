d34a118782d2d34904d6cb3e8a68d223271fa141
Aya Hakimi <ayhakimi@e1r2p7.1337.ma>
2024-10-30 15:06:10 +0100

    lib

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,7 +1,38 @@
 #include "libft.h"
 
+#include <stdlib.h>
+char *ft_strtrim(char const *s1, char const *set) 
+{
+    int i;
+    int l;
+    int n;
 
-char *ft_strtrim(char const *s1, char const *set) {
-
-  
+    i = 0;
+    l = ft_strlen(s1);
+    n = ft_strlen(set);
+    while(s1[i] && s1[i] == set[i])
+    i++;
+    l--;
+    n--;
+    while(l > 0 && s1[l] == set[n])
+    l--;
+    n--;
+    char *p;
+    p = malloc(sizeof(char) * (l - i + 1));
+    if(p == NULL)
+    return NULL;
+    n =0;
+    while(n < l - i)
+    {
+        p[n] = s1[i + n];
+        n++;
+    }
+    p[n] = '\0';
+    return (p);
+}
+    #include <stdio.h>
+    int main()
+    {
+      char *s = "hi lololol hi";
+      printf("%s\n",ft_strtrim(s,"hi"));
     }
