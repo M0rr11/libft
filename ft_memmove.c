10b1f1b294e9e5e5ca222c5480e3fc2b155faf90
Nyx <150604259+M0rr11@users.noreply.github.com>
2024-10-30 03:10:52 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -8,9 +8,9 @@ void *ft_memmove(void *dest, const void *src, size_t n)
     i = 0;
     if (d > s) {
       i = n;
-      while (s[i]) {
+      while (s[i] && i > 0) {
         d[i - 1] = s[i - 1];
-        i++;
+        i--;
       }
     }
     else
