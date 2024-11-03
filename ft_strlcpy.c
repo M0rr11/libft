e75de86ca6d030eff2368c83017d5b78c8a51190
M0rr11 <kiuka05kouki@gmail.com>
2024-11-03 21:57:53 +0100

    lib

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,14 +1,20 @@
 #include "libft.h"
 
- size_t strlcpy(char *dst, const char *src, size_t size)
- {
+size_t ft_strlcpy(char *dst, const char *src, size_t size) {
+  size_t i;
+  i = 0;
 
-
- }
+  while (src[i] && i < size - 1) {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return (ft_strlen(src));
+  }
  #include <stdio.h>
  int main()
  {
-    char *dst = "ok";
-    char *src = "hi";
-    printf("%d,");
+    char dst[3];
+    const char *src = "hi";
+    printf("%zu\n",ft_strlcpy(dst,src,1));
  }
