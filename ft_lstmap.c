35f6405c5eccc9b6f595ea0f642d10ee38a16819
M0rr11 <kiuka05kouki@gmail.com>
2024-11-04 20:59:47 +0100

    lib

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,6 +6,30 @@
 /*   By: nyx <nyx@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:47:57 by nyx               #+#    #+#             */
-/*   Updated: 2024/11/04 14:47:58 by nyx              ###   ########.fr       */
+/*   Updated: 2024/11/04 20:58:57 by nyx              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)) {
+  t_list *list;
+  t_list *tmp;
+  t_list *node;
+
+  tmp = lst;
+  list = NULL;
+  node = NULL;
+  if (!lst || !f || !del)
+    return NULL;
+  while (tmp) {
+    node = ft_lstnew(f(lst->content));
+    if (!node) {
+      ft_lstclear(&list, del);
+      return NULL;
+    }
+    ft_lstadd_back(&list,node);
+    tmp = tmp->next;
+  }
+  return list;
+}
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: nyx <nyx@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:46:45 by nyx               #+#    #+#             */
-/*   Updated: 2024/11/04 14:46:46 by nyx              ###   ########.fr       */
+/*   Updated: 2024/11/04 20:53:05 by nyx              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *node;
 	node = (t_list *)malloc(sizeof(t_list));
-
+    if (!node)
+        return NULL;
 	node->content = content;
 	node->next = NULL;
-
 	return (node);
 }
